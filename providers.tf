e434d7bc2e0fc4e21b549754f0fa8f461feda094
Timothy-py <adeyeyetimothy33@gmail.com>
2023-01-19 23:11:41 +0100

    initialized terraform for aws provider

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+#   required_version = ">= 0.12"
+    required_providers {
+    #   aws = ">= version"
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+    
+}
+
+# Configure the AWS provider
+provider "aws" {
+    region                  = "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+    profile                 = "Learning"
+}
